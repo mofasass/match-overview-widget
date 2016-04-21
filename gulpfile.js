af9e65c519e8be03794fb005f68e97173a744961
Andrei Teodorescu <andrei@globalmouth.com>
2016-04-21 14:38:39 +0200

    Added basic structure, temporary template html, scss

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+(function () {
+   'use strict';
+
+   var gulp = require('gulp');
+
+   var requireDir = require('require-dir');
+
+   var dir = requireDir('./node_modules/widget-build-tools/');
+
+   // Add or override your gulp tasks here
+
+}).call(this);
