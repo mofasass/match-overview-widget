73518a2765d59d9f857b733f1c392bd65216c531
Michael Blom <michael@globalmouth.com>
2016-04-22 14:34:11 +0200

    Filtered out events based on criterions for groups, top scorers and tournament winner

--- src/js/app.js
+++ src/js/app.js
@@ -25,36 +25,84 @@
 
          // Get the betoffers
          var betofferPromise = new Promise(function ( resolve, reject ) {
-            resolve();
+            CoreLibrary.offeringModule.getEventsByFilter('football/euro_2016_tournament_bets/all/all/competitions/')
+               .then(function ( response ) {
+                  resolve(response);
+               }.bind(this));
          }.bind(this));
 
          // When both data fetching promises are resolved, we can create the modules and send them the data
          Promise.all([eventsPromise, betofferPromise])
             .then(function ( promiseData ) {
 
-               console.debug('both promises resolved');
 
                var liveUpcoming = new LiveUpcoming({
                   rootElement: 'section#live-upcoming',
                   events: promiseData[0]
                });
 
-               var groups = new Groups({
-                  rootElement: 'section#groups'
-               });
+               var filteredEvents = this.filterOutBetOffers(promiseData[1].events)
 
-               var topScorer = new TopScorer({
-                  rootElement: 'section#top-scorer'
-               });
+               if ( filteredEvents.groups != null ) {
+                  var groups = new Groups({
+                     rootElement: 'section#groups',
+                     events: filteredEvents.groups
+                  });
+               }
 
-               var tournamentWinner = new TournamentWinner({
-                  rootElement: 'section#tournament-winner'
-               });
+               if ( filteredEvents.topScorer != null ) {
+                  var topScorer = new TopScorer({
+                     rootElement: 'section#top-scorer',
+                     events: filteredEvents.topScorer
+                  });
+               }
+
+               if ( filteredEvents.tournamentWinner != null ) {
+                  var tournamentWinner = new TournamentWinner({
+                     rootElement: 'section#tournament-winner',
+                     events: filteredEvents.tournamentWinner
+                  });
+               }
 
             }.bind(this));
 
-      }
+      },
 
+      /**
+       * Goes through an array of events filters out the events with betoffers that can be mapped based on their criterion id
+       * @param {Array} events An array of event objects containing events and betOffers
+       * @returns {{groups: Array, topScorer: Array, tournamentWinner: Array}}
+       */
+      filterOutBetOffers: function ( events ) {
+         // Map the criterion
+         var mappings = {
+            1001615382: 'groups',
+            1001304945: 'topScorer',
+            1001221607: 'tournamentWinner'
+         };
+
+         // The return object
+         var ret = {
+            groups: [],
+            topScorer: [],
+            tournamentWinner: []
+         };
+
+         // Iterate over the events array
+         var i = 0, len = events.length;
+         for ( ; i < len; ++i ) {
+            // Check if the event has one and only one betOffer
+            if ( events[i].betOffers != null && events[i].betOffers.length === 1 ) {
+               // Check if the criterion id is one we've mapped
+               if ( mappings.hasOwnProperty(events[i].betOffers[0].criterion.id) ) {
+                  // If it is, add it to the return object
+                  ret[mappings[events[i].betOffers[0].criterion.id]].push(events[i]);
+               }
+            }
+         }
+
+         return ret;
+      }
    });
 
    var euroOverview = new EuroOverview({
