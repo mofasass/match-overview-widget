6c910a312ffe5470cb04376a5f4f91542ecd343f
Michael Blom <michael@globalmouth.com>
2016-04-28 17:20:17 +0200

    Updated outcome component

--- src/js/components/OutcomeComponentNoLabel.js
+++ src/js/components/OutcomeComponentNoLabel.js
@@ -2,15 +2,29 @@
 
    var OutcomeViewController = function ( attributes ) {
       this.data = attributes;
+      this.selected = false;
 
-      this.toggleOutcome = function (event, scope) {
-         CoreLibrary.widgetModule.addOutcomeToBetslip(scope.data.outcomeAttr.id);
+      Stapes.on('OUTCOME:ADDED:' + this.data.outcomeAttr.id, ( data, event ) => {
+         this.selected = true;
+      });
+
+      Stapes.on('OUTCOME:REMOVED:' + this.data.outcomeAttr.id, ( data, event ) => {
+         this.selected = false;
+      });
+
+      this.toggleOutcome = function ( event, scope ) {
+         if ( scope.selected === false ) {
+            CoreLibrary.widgetModule.addOutcomeToBetslip(scope.data.outcomeAttr.id);
+         } else {
+            CoreLibrary.widgetModule.removeOutcomeFromBetslip(scope.data.outcomeAttr.id);
+         }
       };
    };
 
    rivets.components['outcome-component-no-label'] = {
       template: function () {
          return '<button rv-on-click="toggleOutcome" rv-disabled="betOffer.suspended | == true"' +
+            'rv-custom-class="selected" rv-toggle-class="KambiWidget-outcome--selected" ' +
             'type="button" role="button" class="KambiWidget-outcome kw-link l-ml-6">' +
             '<div class="l-flexbox l-pack-center">' +
             '<div class="KambiWidget-outcome__odds-wrapper">' +
