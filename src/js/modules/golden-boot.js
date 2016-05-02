5420f363aca88ed01da5eaca0a13354d3c955dff
Michael Blom <michael@globalmouth.com>
2016-05-02 16:55:31 +0200

    Changed some property names in golden boot module

--- src/js/modules/golden-boot.js
+++ src/js/modules/golden-boot.js
@@ -8,20 +8,20 @@ var GoldenBoot = (function () {
          CoreLibrary.Component.apply(this, [{
             rootElement: htmlElement
          }]);
-         this.scope.event = event.betOffers[0].outcomes;
+         this.scope.outcomes = event.betOffers[0].outcomes;
 
          // Assign an index number for each outcome
-         var i = 0, arrLength = this.scope.event.length;
+         var i = 0, arrLength = this.scope.outcomes.length;
          for ( ; i < arrLength; ++i ) {
-            var item = this.scope.event[i];
+            var item = this.scope.outcomes[i];
             if ( typeof item === 'object' ) {
-               item.index = this.scope.event.indexOf(item);
+               item.index = this.scope.outcomes.indexOf(item);
             }
          }
       },
 
       init: function () {
-         this.pagination = new CoreLibrary.PaginationComponent('#golden-boot-pagination', this.scope, 'event', 9);
+         this.pagination = new CoreLibrary.PaginationComponent('#golden-boot-pagination', this.scope, 'outcomes', 9);
       }
    });
 })();
