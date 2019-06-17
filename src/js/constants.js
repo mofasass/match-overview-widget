23563c45f0c70d47ba27e711df4e8cc3436dbf15
Alexey Svirschevskiy <alexey@globalmouth.com>
2019-06-17 17:07:45 +0200

    added new supported filters

--- src/js/constants.js
+++ src/js/constants.js
@@ -28,6 +28,14 @@ export const footballFilters = [
   '/football/england/the_championship',
   '/football/england/league_one',
   '/football/england/league_two',
+  "football/euro_2020_qualification",
+  "football/uefa_championship_u21",
+  "football/copa_america ",
+  "football/world_cup__w_",
+  "football/euro_2020",
+  "football/african_nations_cup",
+  "football/copa_libertadores",
+  "football/world_cup_2022"
 ]
 
 export const americanFootballFilters = [
